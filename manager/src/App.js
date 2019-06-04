7445cc2a9e85ece53293bcaca1d6dd3c4699f6ad
Vashanth <43083250+Vashanth@users.noreply.github.com>
2019-06-04 18:15:58 +0530

    Update App.js

--- manager/src/App.js
+++ manager/src/App.js
@@ -16,13 +16,7 @@ class App extends Component
     componentWillMount()
     {
         firebase.initializeApp({
-            apiKey: "AIzaSyCVwAYuSWIw4Otfl67Sehpz9G6AbwBKV3c",
-            authDomain: "manager-65190.firebaseapp.com",
-            databaseURL: "https://manager-65190.firebaseio.com",
-            projectId: "manager-65190",
-            storageBucket: "manager-65190.appspot.com",
-            messagingSenderId: "80985290403",
-            appId: "1:80985290403:web:a0066151dca918cc"
+        /* Put your firebase details here */
         });
     }
 
